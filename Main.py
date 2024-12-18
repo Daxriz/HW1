f4b56cd8cfe44a0c8d90fdad668b3c0832e0ac37
Daxriz <sanya_akopyan03@mail.ru>
2024-12-18 09:08:00 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ import sys
 import zipfile
 import os
 import json
-from vr import VirtualFileSystem
-from emulator import CommandHandler
+from vr import VirtualFileSystem  
+from emulator import CommandHandler  
 from logger import Logger
 
 def main():
